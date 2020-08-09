494a089801cbe22b06f53dbdc6973c40ef81ed7e
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-09 16:43:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,3 @@
 
 
 
-
-def my_each (array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| "Hello #{i} what's up?"}
